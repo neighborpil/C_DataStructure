c3a8017bd1089f01e366f55a37f2c60983d1f403
neighborpil <neighborpil@naver.com>
2020-01-05 01:54:57 +0900

    radix sort finished

--- CH10Exes/RadixSort/RadixSort.c
+++ CH10Exes/RadixSort/RadixSort.c
@@ -3,6 +3,25 @@
 
 #define BUCKET_NUM 10
 
+void RadixSort(int arr[], int num, int maxLen);
+
+int main(void)
+{
+	int arr[7] = { 13, 212, 14, 7141, 10987, 6, 15 };
+
+	int len = sizeof(arr) / sizeof(int);
+
+	RadixSort(arr, len, 5);
+
+	for (int i = 0; i < len; i++)
+		printf("%d ", arr[i]);
+
+	printf("\n");
+
+	return 0;
+}
+
+
 void RadixSort(int arr[], int num, int maxLen)
 {
 	// 매개변수 maxLen에는 정렬대상 중 가장 긴 데이터의 길이 정보가 전달
@@ -16,7 +35,7 @@ void RadixSort(int arr[], int num, int maxLen)
 		QueueInit(&buckets[bi]);
 
 	// 가장 긴 데이터의 길이만큼 반복
-	for (int pos; pos < maxLen; pos++)
+	for (int pos=0; pos < maxLen; pos++)
 	{
 		// 정렬대상의 수만큼 반복
 		for (int di = 0; di < num; di++)
@@ -29,6 +48,14 @@ void RadixSort(int arr[], int num, int maxLen)
 		}
 
 		// 버킷 수만큼 반복
-		for()
+		for (int bi = 0, di = 0; bi < BUCKET_NUM; bi++)
+		{
+			// 버킷에 저장된 것 순서대로 다 꺼내서 다시 arr에 저장
+			while (!QIsEmpty(&buckets[bi]))
+				arr[di++] = Dequeue(&buckets[bi]);
+		}
+
+		// N번째 자리의 숫자 추출을 위한 피제수의 증가
+		divfac *= 10;
 	}
 }
